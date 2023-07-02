30c5963c14f52a9f9407fe7e5be49452f7054107
Grayson Gullion <xinflux@gmail.com>
2023-07-02 11:01:32 -0400

    make the splash screen not blind the user

--- app/src/main/java/com/blanktheevil/mangareader/MainActivity.kt
+++ app/src/main/java/com/blanktheevil/mangareader/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.blanktheevil.mangareader.data.settings.SettingsManager
 import com.blanktheevil.mangareader.navigation.PrimaryNavGraph
 import com.blanktheevil.mangareader.ui.theme.MangaReaderTheme
@@ -30,6 +31,8 @@ class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalAnimationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
 
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
 
